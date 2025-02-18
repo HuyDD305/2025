cb50098a08c1a4699de55b87c8316c3ebc24c518
HuyDD305 <bribribridu@gmail.com>
2025-02-18 12:02:34 +0700

    ignore 2024

---
+++ HackerRank/soup.py
@@ -0,0 +1,11 @@
+import urllib
+from bs4 import BeautifulSoup
+import ssl
+
+#ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = input("Enter - ")
+html = urllib.request
